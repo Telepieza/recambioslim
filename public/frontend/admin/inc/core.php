28a458e35e62b5d991ed360c8230ce2ad10db327
Telepieza <mariano@telepieza.com>
2023-09-11 01:11:55 +0200

    version 1.0.8

--- public/frontend/admin/inc/core.php
+++ public/frontend/admin/inc/core.php
@@ -19,19 +19,16 @@ if ($core == 'readId') {
   include_once 'readId.php';
   include_once 'action.php';
 } elseif ($core == 'readAll') {
+  $urlParent   = $urlWebClient . $pathWebClient.            // example : https://www.telepieza.com/recambios/frontend/
+  $pageAction  = $urlParent . $pageCreate . $actionReadId ; // example : https://www.telepieza.com/recambios/frontend/tax_ruleForm.php?action=Read&id=  
+  $pageCreate .= $actionCreate;
   include_once 'getAction.php';
   include_once 'readAll.php';
-  include_once 'action.php';
 }
-
+  
 if (!empty($core)) {
-   $urlParent   = $urlWebClient . $pathWebClient.            // https://www.telepieza.com/recambios/frontend/
-   $pageAction  = $urlParent . $pageCreate . $actionReadId ; // https://www.telepieza.com/recambios/frontend/tax_ruleForm.php?action=Read&id=
-   $pageCreate .= $actionCreate;                             // tax_ruleForm.php?action=create
-   $urlParent  .= $pageParent ;                              // https://www.telepieza.com/recambios/frontend/tax_ruleRead.php
-
-   include_once $ruteTheme.'header.php';
-   include_once $ruteTheme.'navbar.php';
+  include_once $ruteTheme.'header.php';
+  include_once $ruteTheme.'navbar.php';
 }
 
 ?>
